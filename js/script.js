76fe67d5d4689e42fba7194b41f2ec5b03ea627e
Frankosviz <geom.francescopaolo@gmail.com>
2024-04-05 00:49:52 +0200

    Boolzapp : coding

--- js/script.js
+++ js/script.js
@@ -1,23 +1,52 @@
-import {contacts} from './data.js';
+import { contacts } from './data.js';
+
+// Link alla libreria per Luxon per la formattazione di date orari ecc...
+
+const dt = luxon.DateTime;
 
 const { createApp } = Vue;
 
 createApp({
-    data(){
+    data() {
         return {
             contacts: contacts,
-            activeContactId: 1
+            activeContactId: 1,
+            messageText: '',
+            searchText: ''
         }
     },
     methods: {
-
+        setActiveContact(id) {
+            this.activeContactId = id;
+        },
+        sendMessage() {
+            const newMessage = {
+                date: new Date().toLocaleString(),
+                message: this.messageText,
+                status: 'sent'
+            }
+            this.activeContact.messages.push(newMessage);
+            this.messageText = '';
+            setTimeout(() => {
+                const newMessage = {
+                    // Utilizziamo il metodo dt che abbiamo creato sopra per la restituzione dell'ora di questo momento
+                    date: dt.now().setLocale('it').toFormat('dd/MM/yyyy HH:mm:ss'),
+                    message: 'ok',
+                    status: 'received'
+                }
+                this.activeContact.messages.push(newMessage);
+            }, 1000);
+        },
     },
-    mounted(){
+    mounted() {
         console.log(this.contacts)
     },
     computed: {
-        activeContact(){
+        activeContact() {
             return this.contacts.find(contact => contact.id === this.activeContactId)
+        },
+        filteredContacts() {
+            return this.contacts.filter((el)=> el.name.toLowerCase().includes(this.searchText.toLowerCase()));
         }
     }
 }).mount('#app');
